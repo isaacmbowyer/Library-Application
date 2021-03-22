ba04948ae8bd22c964c9b20d095be10bf75473c2
Isaac Bowyer <c0007135@my.shu.ac.uk>
2021-03-22 21:14:02 +0000

    Commit Database

--- LibraryLove/Startup.cs
+++ LibraryLove/Startup.cs
@@ -24,7 +24,6 @@ namespace LibraryLove
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
