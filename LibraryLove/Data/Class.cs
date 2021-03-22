7ae65eae0413c439a020a976fe5627cd128b2572
Isaac Bowyer <c0007135@my.shu.ac.uk>
2021-03-22 20:20:04 +0000

    Added Folder

---
+++ LibraryLove/Data/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryLove.Data
+{
+    public class Class
+    {
+        Console.WriteLine('hello');
+    }
+}
